15d3fabc083f71eaa11af3f88a7871e57d7e8e70
Lucas Dal Grande Ghizoni <lucas.ghizoni@digitro.com.br>
2017-06-19 13:25:47 -0300

    pagination

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 class Grid extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.handleClickNext = this.handleClickNext.bind(this);
+    this.handleClickPrevious = this.handleClickPrevious.bind(this);
+  }
+
   render() {
 
   	var rows = this.props.items.map(function(item) {
@@ -14,16 +21,33 @@ class Grid extends Component {
     });
 
     return (
+      <div>
         <table className="table table-hover">
           <thead>
               <tr>
-                  <th>Title</th>
-                  <th>Author</th>
+                <th>Title</th>
+                <th>Author</th>
               </tr>
           </thead>
           <tbody>{rows}</tbody>
         </table>
+        <button onClick={this.handleClickPrevious}>Previous</button>
+        <button onClick={this.handleClickNext}>Next</button>
+      </div>
     );
+  }
+
+  handleClickNext(){
+    debugger;
+    fetch(this.props.url + "&startIndex=40").then(response => {
+      response.json().then(data => {
+        this.props.items = data.items;
+      });
+    });
+  }
+
+  handleClickPrevious(){
+
   }
 }
 
